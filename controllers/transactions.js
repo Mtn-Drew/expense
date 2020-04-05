d8315167857b11a8bdedb5593c2ea3b7da298fab
Drew Carrillo <drewc3@gmail.com>
2020-04-05 16:04:32 -0600

    connect db

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,7 +1,19 @@
 // @desc Get all transactions
-// @route /api/v1/transactions
+// @route GET /api/v1/transactions
 // @access Public
-
 exports.getTransactions = (req, res, next) => {
   res.send('GET transactions')
+}
+
+// @desc Add transaction
+// @route POST /api/v1/transactions
+// @access Public
+exports.addTransaction = (req, res, next) => {
+  res.send('POST transactions')
+}
+// @desc DELETE transaction
+// @route DELETE /api/v1/transactions:id
+// @access Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send('DELETE transaction')
 }
